e81c4668d07c37fc7fe1a2f8d93e94bc3832c2a9
Clayton Mercer <mercerccw@gmail.com>
2021-01-24 01:58:36 -0500

    Add project thumbnails

--- src/models/project.js
+++ src/models/project.js
@@ -1,325 +1,325 @@
 const projects = [
-  {
-    "id": 1,
-    "name": "Cardinal AR",
-    "description": "An augmented reality app built in Unity3D.",
-    "role": "Lead Developer",
-    "type": "iOS/Android app",
-    "featured": true,
-    "thumbnail": "https://wallpaperaccess.com/full/38580.jpg",
-    "tools": [
-      {
-        "name": "Blender"
-      },
-      {
-        "name": "Unity3D"
-      }
-    ],
-    "languages": [
-      {
-        "name": "C#"
-      }
-    ],
-    "links": [
-      {
-        "name": "App Store",
-        "link": "https://www.google.com/"
-      },
-      {
-        "name": "Google Play Store",
-        "link": "https://www.google.com/"
-      },
-    ]
-  },
-  {
-    "id": 2,
-    "name": "Chirper App",
-    "description": "An app for showing Ball State Cardinal Spirit",
-    "role": "Developer",
-    "type": "iOS/Android app",
-    "featured": false,
-    "thumbnail": "https://wallpaperaccess.com/full/38580.jpg",
-    "tools": [
-      {
-        "name": "Google Analytics"
-      },
-      {
-        "name": "React Native"
-      }
-    ],
-    "languages": [
-      {
-        "name": "JavaScript"
-      }
-    ],
-    "links": [
-      {
-        "name": "App Store",
-        "link": "https://www.google.com/"
-      },
-      {
-        "name": "Google Play Store",
-        "link": "https://www.google.com/"
-      },
-    ]
-  },
-  {
-    "id": 3,
-    "name": "Equipment Checkout System",
-    "description": "It's a project for checking things out",
-    "role": "Developer",
-    "type": "Website",
-    "featured": false,
-    "thumbnail": "https://i.pinimg.com/originals/2f/e8/05/2fe8053c01e6dbc40451832248df5e4f.jpg",
-    "tools": [
-      {
-        "name": "AngularJS"
-      },
-      {
-        "name": "MySQL"
-      },
-      {
-        "name": "SLIM"
-      }
-    ],
-    "languages": [
-      {
-        "name": "CSS"
-      },
-      {
-        "name": "HTML"
-      },
-      {
-        "name": "JavaScript"
-      },
-      {
-        "name": "PHP"
-      },
-      {
-        "name": "SQL"
-      }
-    ],
-    "links": []
-  },
-  {
-    "id": 4,
-    "name": "Immersive Learning Brochure",
-    "description": "A promotional brochure made to market the Digital Corps",
-    "role": "Project Manager",
-    "type": "Brochure",
-    "featured": false,
-    "thumbnail": "https://i.pinimg.com/originals/2f/e8/05/2fe8053c01e6dbc40451832248df5e4f.jpg",
-    "tools": [],
-    "languages": [],
-    "links": []
-  },
-  {
-    "id": 5,
-    "name": "Life-Extend",
-    "description": "A health and wellness tracking application build in React Native.",
-    "role": "Developer",
-    "type": "iOS/Android app",
-    "featured": true,
-    "thumbnail": "https://www.wallpapertip.com/wmimgs/3-30668_3840x2160-landscape-4k-ultra-hd-wallpaper-ultra-hd.jpg",
-    "tools": [
-      {
-        "name": "GraphQL"
-      },
-      {
-        "name": "Jest"
-      },
-      {
-        "name": "React Native"
-      },
-      {
-        "name": "Redux"
-      }
-    ],
-    "languages": [
-      {
-        "name": "TypeScript"
-      }
-    ],
-    "links": [
-      {
-        "name": "App Store",
-        "link": "https://www.google.com/"
-      },
-      {
-        "name": "Google Play Store",
-        "link": "https://www.google.com/"
-      },
-    ]
-  },
-  {
-    "id": 6,
-    "name": "Philosophy Outreach Site",
-    "description": "A promotional website designed for the Ball State Philosophy outreach program.",
-    "role": "Developer",
-    "type": "Website",
-    "featured": false,
-    "thumbnail": "https://www.wallpapertip.com/wmimgs/3-30668_3840x2160-landscape-4k-ultra-hd-wallpaper-ultra-hd.jpg",
-    "tools": [
-      {
-        "name": "Wordpress"
-      }
-    ],
-    "languages": [
-      {
-        "name": "CSS"
-      },
-      {
-        "name": "HTML"
-      },
-      {
-        "name": "JavaScript"
-      },
-      {
-        "name": "PHP"
-      }
-    ],
-    "links": [
-      {
-        "name": "Philosophy Outreach",
-        "link": "https://philosophyoutreachproject.azurewebsites.net/"
-      }
-    ]
-  },
-  {
-    "id": 7,
-    "name": "Project Dashboard",
-    "description": "A project management website designed to manage projects.",
-    "role": "Developer",
-    "type": "Website",
-    "featured": false,
-    "thumbnail": "https://www.wallpapertip.com/wmimgs/3-30668_3840x2160-landscape-4k-ultra-hd-wallpaper-ultra-hd.jpg",
-    "tools": [
-      {
-        "name": "React"
-      },
-      {
-        "name": "SLIM"
-      },
-      {
-        "name": "MySQL"
-      }
-    ],
-    "languages": [
-      {
-        "name": "CSS"
-      },
-      {
-        "name": "HTML"
-      },
-      {
-        "name": "JavaScript"
-      },
-      {
-        "name": "PHP"
-      },
-      {
-        "name": "SQL"
-      },
-    ],
-    "links": []
-  },
-  {
-    "id": 8,
-    "name": "Remote Teaching Site",
-    "description": "A instructional website built to teach teachers how to run classes in the context of remote work.",
-    "role": "Developer",
-    "type": "Website",
-    "featured": false,
-    "thumbnail": "https://www.wallpapertip.com/wmimgs/3-30668_3840x2160-landscape-4k-ultra-hd-wallpaper-ultra-hd.jpg",
-    "tools": [
-      {
-        "name": "Wordpress"
-      }
-    ],
-    "languages": [
-      {
-        "name": "CSS"
-      },
-      {
-        "name": "HTML"
-      },
-      {
-        "name": "JavaScript"
-      },
-      {
-        "name": "PHP"
-      }
-    ],
-    "links": [
-      {
-        "name": "Teaching Remotely",
-        "link": "https://remoteteaching.bsu.edu/"
-      }
-    ]
-  },
-  {
-    "id": 9,
-    "name": "Trust Admin App",
-    "description": "A trust account management app built using Microsoft Xamarin",
-    "role": "Developer",
-    "type": "iOS/Android app",
-    "featured": true,
-    "thumbnail": "https://www.wallpapertip.com/wmimgs/3-30668_3840x2160-landscape-4k-ultra-hd-wallpaper-ultra-hd.jpg",
-    "tools": [
-      {
-        "name": "Xamarin"
-      }
-    ],
-    "languages": [
-      {
-        "name": "C#"
-      }
-    ],
-    "links": []
-  },
-  {
-    "id": 10,
-    "name": "Writing Challenge Site",
-    "description": "A website meant for Ball State staff members to compete in logging consistent time spent writing.",
-    "role": "Developer",
-    "type": "Website",
-    "featured": false,
-    "thumbnail": "https://www.wallpapertip.com/wmimgs/3-30668_3840x2160-landscape-4k-ultra-hd-wallpaper-ultra-hd.jpg",
-    "tools": [
-      {
-        "name": "MySQL"
-      },
-      {
-        "name": "React"
-      },
-      {
-        "name": "Slim"
-      }
-    ],
-    "languages": [
-      {
-        "name": "CSS"
-      },
-      {
-        "name": "HTML"
-      },
-      {
-        "name": "JavaScript"
-      },
-      {
-        "name": "PHP"
-      },
-      {
-        "name": "SQL"
-      },
-    ],
-    "links": [
-      {
-        "name": "BSU Writing Challenge",
-        "link": "https://writingchallenge.bsu.edu/"
-      }
-    ]
-  }
+    {
+        "id": 1,
+        "name": "Cardinal AR",
+        "description": "An augmented reality app built in Unity3D.",
+        "role": "Lead Developer",
+        "type": "iOS/Android app",
+        "featured": true,
+        "thumbnail": "https://mercer-portfolio-assets.s3.us-east-2.amazonaws.com/cardinal-ar-ball-state.jpg",
+        "tools": [
+            {
+                "name": "Blender"
+            },
+            {
+                "name": "Unity3D"
+            }
+        ],
+        "languages": [
+            {
+                "name": "C#"
+            }
+        ],
+        "links": [
+            {
+                "name": "App Store",
+                "link": "https://apps.apple.com/us/app/cardinal-ar-ball-state/id1481248718"
+            },
+            {
+                "name": "Google Play Store",
+                "link": "https://play.google.com/store/apps/details?id=edu.bsu.digitalcorps.apps.cardinalar"
+            },
+        ]
+    },
+    {
+        "id": 2,
+        "name": "Chirper App",
+        "description": "An app for showing Ball State Cardinal Spirit",
+        "role": "Developer",
+        "type": "iOS/Android app",
+        "featured": false,
+        "thumbnail": "https://mercer-portfolio-assets.s3.us-east-2.amazonaws.com/chirper-app-home.jpg",
+        "tools": [
+            {
+                "name": "Google Analytics"
+            },
+            {
+                "name": "React Native"
+            }
+        ],
+        "languages": [
+            {
+                "name": "JavaScript"
+            }
+        ],
+        "links": [
+            {
+                "name": "App Store",
+                "link": "https://apps.apple.com/us/app/chirper/id1370886908"
+            },
+            {
+                "name": "Google Play Store",
+                "link": "https://play.google.com/store/apps/details?id=edu.bsu.digitalcorps.chirper"
+            },
+        ]
+    },
+    {
+        "id": 3,
+        "name": "Equipment Checkout System",
+        "description": "It's a project for checking things out",
+        "role": "Developer",
+        "type": "Website",
+        "featured": false,
+        "thumbnail": "https://mercer-portfolio-assets.s3.us-east-2.amazonaws.com/equipment-checkout-site.png",
+        "tools": [
+            {
+                "name": "AngularJS"
+            },
+            {
+                "name": "MySQL"
+            },
+            {
+                "name": "SLIM"
+            }
+        ],
+        "languages": [
+            {
+                "name": "CSS"
+            },
+            {
+                "name": "HTML"
+            },
+            {
+                "name": "JavaScript"
+            },
+            {
+                "name": "PHP"
+            },
+            {
+                "name": "SQL"
+            }
+        ],
+        "links": []
+    },
+    {
+        "id": 4,
+        "name": "Immersive Learning Brochure",
+        "description": "A promotional brochure made to market the Digital Corps",
+        "role": "Project Manager",
+        "type": "Brochure",
+        "featured": false,
+        "thumbnail": "https://mercer-portfolio-assets.s3.us-east-2.amazonaws.com/immersive-learning-brochure.PNG",
+        "tools": [],
+        "languages": [],
+        "links": []
+    },
+    {
+        "id": 5,
+        "name": "Life-Extend",
+        "description": "A health and wellness tracking application build in React Native.",
+        "role": "Developer",
+        "type": "iOS/Android app",
+        "featured": true,
+        "thumbnail": "https://mercer-portfolio-assets.s3.us-east-2.amazonaws.com/life-extend-home.jpg",
+        "tools": [
+            {
+                "name": "GraphQL"
+            },
+            {
+                "name": "Jest"
+            },
+            {
+                "name": "React Native"
+            },
+            {
+                "name": "Redux"
+            }
+        ],
+        "languages": [
+            {
+                "name": "TypeScript"
+            }
+        ],
+        "links": [
+            {
+                "name": "App Store",
+                "link": "https://www.google.com/"
+            },
+            {
+                "name": "Google Play Store",
+                "link": "https://www.google.com/"
+            },
+        ]
+    },
+    {
+        "id": 6,
+        "name": "Philosophy Outreach Site",
+        "description": "A promotional website designed for the Ball State Philosophy outreach program.",
+        "role": "Developer",
+        "type": "Website",
+        "featured": false,
+        "thumbnail": "https://mercer-portfolio-assets.s3.us-east-2.amazonaws.com/Philosophy-site-home.png",
+        "tools": [
+            {
+                "name": "Wordpress"
+            }
+        ],
+        "languages": [
+            {
+                "name": "CSS"
+            },
+            {
+                "name": "HTML"
+            },
+            {
+                "name": "JavaScript"
+            },
+            {
+                "name": "PHP"
+            }
+        ],
+        "links": [
+            {
+                "name": "Philosophy Outreach",
+                "link": "https://philosophyoutreachproject.azurewebsites.net/"
+            }
+        ]
+    },
+    {
+        "id": 7,
+        "name": "Project Dashboard",
+        "description": "A project management website designed to manage projects.",
+        "role": "Developer",
+        "type": "Website",
+        "featured": false,
+        "thumbnail": "https://mercer-portfolio-assets.s3.us-east-2.amazonaws.com/project-dashboard.png",
+        "tools": [
+            {
+                "name": "React"
+            },
+            {
+                "name": "SLIM"
+            },
+            {
+                "name": "MySQL"
+            }
+        ],
+        "languages": [
+            {
+                "name": "CSS"
+            },
+            {
+                "name": "HTML"
+            },
+            {
+                "name": "JavaScript"
+            },
+            {
+                "name": "PHP"
+            },
+            {
+                "name": "SQL"
+            },
+        ],
+        "links": []
+    },
+    {
+        "id": 8,
+        "name": "Remote Teaching Site",
+        "description": "A instructional website built to teach teachers how to run classes in the context of remote work.",
+        "role": "Developer",
+        "type": "Website",
+        "featured": false,
+        "thumbnail": "https://mercer-portfolio-assets.s3.us-east-2.amazonaws.com/remote-teaching-site.png",
+        "tools": [
+            {
+                "name": "Wordpress"
+            }
+        ],
+        "languages": [
+            {
+                "name": "CSS"
+            },
+            {
+                "name": "HTML"
+            },
+            {
+                "name": "JavaScript"
+            },
+            {
+                "name": "PHP"
+            }
+        ],
+        "links": [
+            {
+                "name": "Teaching Remotely",
+                "link": "https://remoteteaching.bsu.edu/"
+            }
+        ]
+    },
+    {
+        "id": 9,
+        "name": "Trust Admin App",
+        "description": "A trust account management app built using Microsoft Xamarin",
+        "role": "Developer",
+        "type": "iOS/Android app",
+        "featured": true,
+        "thumbnail": "https://mercer-portfolio-assets.s3.us-east-2.amazonaws.com/trust-admin-app-home.jpg",
+        "tools": [
+            {
+                "name": "Xamarin"
+            }
+        ],
+        "languages": [
+            {
+                "name": "C#"
+            }
+        ],
+        "links": []
+    },
+    {
+        "id": 10,
+        "name": "Writing Challenge Site",
+        "description": "A website meant for Ball State staff members to compete in logging consistent time spent writing.",
+        "role": "Developer",
+        "type": "Website",
+        "featured": false,
+        "thumbnail": "https://mercer-portfolio-assets.s3.us-east-2.amazonaws.com/writing-challeng-home.png",
+        "tools": [
+            {
+                "name": "MySQL"
+            },
+            {
+                "name": "React"
+            },
+            {
+                "name": "Slim"
+            }
+        ],
+        "languages": [
+            {
+                "name": "CSS"
+            },
+            {
+                "name": "HTML"
+            },
+            {
+                "name": "JavaScript"
+            },
+            {
+                "name": "PHP"
+            },
+            {
+                "name": "SQL"
+            },
+        ],
+        "links": [
+            {
+                "name": "BSU Writing Challenge",
+                "link": "https://writingchallenge.bsu.edu/"
+            }
+        ]
+    }
 ]
 
 module.exports = projects;
